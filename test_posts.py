35101f723e40c02e3537c7fec637ac5f80b69b15
RonielsonOliveira <ronielsonoliveira@live.com>
2023-05-29 19:08:22 -0300

    fourth commit, all tests made and coverage report

--- test_posts.py
+++ test_posts.py
@@ -80,5 +80,43 @@ class Profiles(unittest.TestCase):
             self.assertIn('body', postList_item)
             self.assertEqual(type(postList_item['body']), str)
 
+    def test_5_put_posts_id(self):
+        body = {
+            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        }
+        response_put = requests.put(f'{self.url}/posts/1', json=body)
+        self.assertEqual(response_put.status_code, 200)
+        json_data_put = json.loads(response_put.text)
+        print(json_data_put)
+        self.assertIn('title', json_data_put)
+        self.assertEqual(type(json_data_put['title']), str)
+        self.assertIn('body', json_data_put)
+        self.assertEqual(type(json_data_put['body']), str)
+        self.assertIn('id', json_data_put)
+        self.assertEqual(type(json_data_put['id']), int)
+
+    def test_6_patch_posts(self):
+        body = {
+            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit, teste",
+            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        }
+        response_patch = requests.patch(f'{self.url}/posts/1', json=body)
+        self.assertEqual(response_patch.status_code, 200)
+        json_data_patch = json.loads(response_patch.text)
+        print(json_data_patch)
+        self.assertIn('title', json_data_patch)
+        self.assertEqual(type(json_data_patch['title']), str)
+        self.assertIn('body', json_data_patch)
+        self.assertEqual(type(json_data_patch['body']), str)
+        self.assertIn('id', json_data_patch)
+        self.assertEqual(type(json_data_patch['id']), int)
+
+    def test_7_delete_post(self):
+        response_delete = requests.delete(f'{self.url}/posts/1')
+        self.assertEqual(response_delete.status_code, 200)
+
+
+
 if __name__ == '__main__':
     unittest.main()
